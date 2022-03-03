f35255196a35a0332f180b3f9e6f432ce3161414
Daniel Interiano <dannyboyint@gmail.com>
2022-03-03 13:30:55 -0500

    Added another migration for my reviews table

--- db/migrate/20220303174902_create_mangas.rb
+++ db/migrate/20220303174902_create_mangas.rb
@@ -1,7 +1,9 @@
 class CreateMangas < ActiveRecord::Migration[6.1]
   def change
     create_table :mangas do |t|
-      
+      t.string :title
+      t.string :author
+      t.string :publisher
     end
   end
 end
